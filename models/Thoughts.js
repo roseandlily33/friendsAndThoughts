11b4e2d324926d9a1a9389b7d63292e721737f7f
Victoria Benoit <vrose834@gmail.com>
2023-05-18 09:21:33 -0300

    Did the thoughts model

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,17 +1,36 @@
 const {Schema, model} = require('mongoose');
+const moment = require('moment');
 
 const thoughtSchema = new Schema ({
     thoughtText: {type: String, required: true, minLength: 1, maxLength: 280},
     createdAt: {type: Date, default: Date.now()},
-    //Username, type: string, required: true, do I need to pull this in?
-    //Reactions, [reactionSchema] 
-    
-}, {
+    username: {type: String, required: true},
+    reactions: [reactionSchema], 
+}, 
+{
     toJSON: {
         virtuals: true,
-        //getters: true?
+        getters: true,
     },
     id: false
-})
+});
 
-//Created at needs a getter method to format the time
+thoughtSchema.virtual('formatDate').get(function(date){
+   return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+});
+
+const reactionSchema = new Schema ({
+    reactionId:{type: Schema.Types.ObjectId, default: Schema.Object.Id},
+    reactionBody:{type: String, required: true, maxLength: 280},
+    username: {type: String, required: true},
+    createdAt:{type: Date, default: Date.now()}
+});
+
+
+reactionSchema.virtual('formatDate').get(function(date){
+    return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+ });
+
+const Reaction = model('Reaction', reactionSchema;)
+const Thought = model('Thought', thoughtSchema);
+module.exports = {Thought, Reaction};
