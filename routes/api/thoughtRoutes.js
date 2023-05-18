8f93154911fe9ad3d8526298c77e187ccde79427
Victoria Benoit <vrose834@gmail.com>
2023-05-18 08:17:18 -0300

    templates for user/thoughts router, server works

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const{
+
+} = require('../../controllers/thoughtContoller');
+
+//api/users
+router.route('/')
+//api/users/:userId
+router.route('/');
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const{
+
+} = require('../../controllers/userController');
+
+router.route('/')
+
+//api/users
+router.route('/')
+//api/users/:userId
+router.route('/');
+
+module.exports = router;
