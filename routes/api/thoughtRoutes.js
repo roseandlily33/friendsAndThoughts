4a84788a03455d486549df7c6a0738b544e8ea53
Victoria Benoit <vrose834@gmail.com>
2023-05-18 09:53:05 -0300

    Connected the thought routes and controllers

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,9 +10,13 @@ const{
     deleteReaction
 } = require('../../controllers/thoughtContoller');
 
-//api/users
-router.route('/')
-//api/users/:userId
-router.route('/');
+//api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+//api/thoughts/:id
+router.route('/:id').get(myThoughts).put(updateThought).delete(deleteThought);
+
+//api/thoughts/:thoughtId/reaction
+router.route('/:id/reaction').post(addReaction).delete(deleteReaction);
 
 module.exports = router;
