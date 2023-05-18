75c17894337b66a3cb9de84214da547122e7c2b7
Victoria Benoit <vrose834@gmail.com>
2023-05-18 08:59:14 -0300

    Finished the User model

--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
 const {Schema, model} = require('mongoose');
+import {isEmail} from 'validator';
 
 const userSchema = new Schema ({
     userName: {type: String, required: true, unique: true, trim: true },
-    email: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, validate:[isEmail, 'Enter a valid Email']},
     thoughts: [{type: Schema.Types.ObjectId, ref: 'Thought'}],
     friends: [{type: Schema.Types.ObjectId, ref: 'User'}]
 }, {
     toJSON: {
         virtuals: true
-    }, id: false
+    }, 
+    id: false
 });
 
-userSchema.virtual('friendCount').get(function(friends){
+userSchema.virtual('friendCount').get(function(){
     return this.friends.length;
 });
 
